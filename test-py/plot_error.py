309ae79238913dd518a0ca2199ba1badec084c82
AnZreww <129043126+An-314@users.noreply.github.com>
2024-03-09 17:49:22 +0800

    1

--- test-py/plot_error.py
+++ test-py/plot_error.py
@@ -5,7 +5,127 @@ matplotlib.use('Agg')  # 设置非交互式后端
 import matplotlib.pyplot as plt
 import updating
 
-def polt_error(m, n, times):
+def polt_error_r(m, n, times):
+    """
+    对一个矩阵不断插入最后一列，然后删除最后一列，画出QR分解的更新误差，绘制：
+    1. A的1-范数误差
+    2. Q.T@Q与1的1-范数误差
+    3. A.T@A与R.T@R的1-范数误差
+    4. R的1-范数误差
+    5. R.T@R与R_updated.T@R_updated的1-范数误差
+
+    Parameters
+    ----------
+    m : int
+        矩阵A的行数
+    n : int
+        矩阵A的列数
+    times : int
+        操作次数
+    """
+    # 生成随机矩阵 A
+    A_updated = np.random.rand(m, n).astype(np.float64)
+    # 建一个表格储存误差
+    count = 0
+    length = 2 * times + 1
+    errors1 = np.zeros((length, 1))
+    errors2 = np.zeros((length, 1))
+    errors3 = np.zeros((length, 1))
+    errors4 = np.zeros((length, 1))
+    errors5 = np.zeros((length, 1))
+    ##### 简略QR分解测试 #####
+    # 计算QR分解
+    Q, R = np.linalg.qr(A_updated, mode="complete")
+    Q = Q.astype(np.float64)
+    R = R.astype(np.float64)
+    count = 0
+    j = n # 插入新列的位置 
+    for i in range(times):
+        j += 1
+        count += 1
+        # 生成随机行向量 x
+        x = np.random.rand(m).astype(np.float64)
+        # 调用 dqrinc 更新 QR 分解
+        Q, R = updating.appending_column(Q, R, j, x)
+        # 验证 QR 分解的正确性
+        A_updated = np.hstack([A_updated[:, :j-1], x.reshape(-1, 1), A_updated[:, j-1:]])
+        A_reconstructed = Q @ R
+        ATA = np.dot(A_updated.T, A_updated)
+        RTR = np.dot(R.T, R)
+        _,R_updated = np.linalg.qr(A_updated, mode="complete") 
+        RuTRu = np.dot(R_updated.T, R_updated)
+        # 计算误差
+        error1 = np.linalg.norm(A_updated - A_reconstructed, 1)
+        error2 = np.linalg.norm(Q.T @ Q - np.eye(Q.shape[1]), 1)
+        error3 = np.linalg.norm(ATA - RTR, 1)
+        error4 = np.linalg.norm(R, 1) - np.linalg.norm(R_updated, 1)
+        error5 = np.linalg.norm(RTR - RuTRu, 1)
+        errors1[count] = error1
+        errors2[count] = error2
+        errors3[count] = error3
+        errors4[count] = error4
+        errors5[count] = error5
+        print(f"finish:{count}")
+    for i in range(times):
+        j -= 1
+        count += 1
+        # 调用 dqrinc 更新 QR 分解
+        Q, R = updating.deleting_column(Q, R, j)
+        # 验证 QR 分解的正确性
+        A_updated = np.hstack([A_updated[:, :j-1], A_updated[:, j:]])
+        A_reconstructed = Q @ R
+        ATA = np.dot(A_updated.T, A_updated)
+        RTR = np.dot(R.T, R)
+        _,R_updated = np.linalg.qr(A_updated, mode="complete") 
+        RuTRu = np.dot(R_updated.T, R_updated)
+        # 计算误差
+        error1 = np.linalg.norm(A_updated - A_reconstructed, 1)
+        error2 = np.linalg.norm(Q.T @ Q - np.eye(Q.shape[1]), 1)
+        error3 = np.linalg.norm(ATA - RTR, 1)
+        error4 = np.linalg.norm(R, 1) - np.linalg.norm(R_updated, 1)
+        error5 = np.linalg.norm(RTR - RuTRu, 1)
+        errors1[count] = error1
+        errors2[count] = error2
+        errors3[count] = error3
+        errors4[count] = error4
+        errors5[count] = error5
+        print(f"finish:{count}")
+    # print(errors)
+    plt.plot(errors1)
+    plt.xlabel("n")
+    plt.ylabel("error")
+    plt.title("1-norm error of A_updated and A_reconstructed:column")
+    plt.savefig(f"1norm_error_plot_A_column.png")
+    # 清空图像
+    plt.cla()
+    plt.plot(errors2)
+    plt.xlabel("n")
+    plt.ylabel("error")
+    plt.title("1-norm error of Q.T@Q and I:column")
+    plt.savefig(f"1norm_error_plot_Q_column.png")
+    # 清空图像
+    plt.cla()
+    plt.plot(errors3)
+    plt.xlabel("n")
+    plt.ylabel("error")
+    plt.title("1-norm error of A.T@A and R.T@R:column")
+    plt.savefig(f"1norm_error_plot_R_column.png")
+    # 清空图像
+    plt.cla()
+    plt.plot(errors4)
+    plt.xlabel("n")
+    plt.ylabel("error")
+    plt.title("1-norm error of R and R_updated:column")
+    plt.savefig(f"1norm_error_plot_R_updated_column.png")
+    # 清空图像
+    plt.cla()
+    plt.plot(errors5)
+    plt.xlabel("n")
+    plt.ylabel("error")
+    plt.title("1-norm error of RTR and R_updated.T@R_updated:column")
+    plt.savefig(f"1norm_error_plot_RTR_column.png")
+
+def polt_error_d(m, n, times):
     """
     对一个矩阵不断插入最后一行，然后删除最后一行，画出QR分解的更新误差，绘制：
     1. A的1-范数误差
@@ -161,6 +281,7 @@ def polt_error_ch(n, times):
         x_history = np.copy(x)
         # 调用 dqrinc 
         R_updated = updating.cholesky_update(R_updated, n, x)
+        print(R_updated.shape)
         # 验证更新后的 R
         A_updated = A_updated + x_history.reshape(-1, 1) @ x_history.reshape(1, -1)
         A_reconstructed = R_updated.T @ R_updated
@@ -205,5 +326,6 @@ def polt_error_ch(n, times):
 
 
 if __name__ == "__main__":
-    polt_error(100, 100, 100)
-    # polt_error_ch(100, 200)
+    polt_error_r(100, 60, 20)
+    polt_error_d(100, 100, 100)
+    polt_error_ch(100, 200)
--- test-py/updating.py
+++ test-py/updating.py
@@ -3,42 +3,146 @@ import sys
 sys.path.append('/home/anzrew/Documents/qrupdate/src/build/lib.linux-x86_64-cpython-311')
 import qrupdate
 
-def appending_row(Q,R,j,x):
+def appending_column(Q, R, j, x):
     """
-    对矩阵A的完整QR分解，传入QR与加入的行x，将x加入A的第j行得到A1，返回A1的QR分解
+    对矩阵A的完全QR分解，传入QR与加入的列x，将x加入A的第j列得到A1，返回A1的QR分解
+
+    Parameters
+    ----------
+    Q : (m, m)
+        正交矩阵
+    R : (m, n)
+        上三角矩阵
+    j : int
+        加入的列的位置
+    x : (m,)
+        加入的列
+
+    Returns
+    -------
+    Q : (m, m)
+        正交矩阵
+    R : (m, n+1)
+        上三角矩阵
+    """
+    m, n = R.shape
+    k = n
+    Q = Q.astype(np.float64, order="F")
+    R = R.astype(np.float64, order="F")
+    x = x.astype(np.float64, order="F")
+    R = np.append(R, np.zeros((m, 1)), axis=1)
+
+    qrupdate.dqrinc(k, Q, R, j, x)
+
+    Q = Q.copy(order="C")
+    R = R.copy(order="C")
+    return Q, R
+
+
+def deleting_column(Q, R, j):
+    """
+    对矩阵A的完全QR分解，传入QR，将A的第j列删除得到A1，返回A1的QR分解
+
+    Parameters
+    ----------
+    Q : (m, m)
+        正交矩阵
+    R : (m, n)
+        上三角矩阵
+    j : int
+        删除的列的位置
+
+    Returns
+    -------
+    Q : (m, m)
+        正交矩阵
+    R : (m, n-1)
+        上三角矩阵
     """
     m, n = R.shape
     Q = Q.astype(np.float64, order="F")
     R = R.astype(np.float64, order="F")
+
+    qrupdate.dqrdec(Q, R, j)
+
+    Q = Q.copy(order="C")
+    R = R.copy(order="C")
+    R = R[:, :-1]
+    return Q, R
+
+
+def appending_row(Q, R, j, x):
+    """
+    对矩阵A的完整QR分解，传入QR与加入的行x，将x加入A的第j行得到A1，返回A1的QR分解
+
+    Parameters
+    ----------
+    Q : (m, m)
+        正交矩阵
+    R : (m, n)
+        上三角矩阵
+    j : int
+        加入的行的位置
+    x : (n,)
+        加入的行
+
+    Returns
+    -------
+    Q : (m+1, m+1)
+        正交矩阵
+    R : (m+1, n)
+        上三角矩阵
+    """
+    m, n = R.shape
+    Q = Q.astype(np.float64, order="F")
+    R = R.astype(np.float64, order="F")
+    x = x.astype(np.float64, order="F")
 
     Q = np.append(Q, np.zeros((1, m)), axis=0)
     Q = np.append(Q, np.zeros((m + 1, 1)), axis=1)
     R = np.append(R, np.zeros((1, n)), axis=0)
 
-    w = np.zeros(2*n).astype(np.float64, order="F")
-    qrupdate.dqrinr(m,Q,R,j,x,w)
+    w = np.zeros(2 * n).astype(np.float64, order="F")
+    qrupdate.dqrinr(m, Q, R, j, x, w)
 
-    Q = Q.copy(order='C')
-    R = R.copy(order='C')
+    Q = Q.copy(order="C")
+    R = R.copy(order="C")
     return Q, R
 
-def deleting_row(Q,R,j):
+
+def deleting_row(Q, R, j):
     """
     对矩阵A的完整QR分解，传入QR，将A的第j行删除得到A1，返回A1的QR分解
+
+    Parameters
+    ----------
+    Q : (m, m)
+        正交矩阵
+    R : (m, n)
+        上三角矩阵
+    j : int
+        删除的行的位置
+
+    Returns
+    -------
+    Q : (m-1, m-1)
+        正交矩阵
+    R : (m-1, n)
+        上三角矩阵
     """
     m, n = R.shape
     Q = Q.astype(np.float64, order="F")
     R = R.astype(np.float64, order="F")
 
-    w = np.zeros(2*m).astype(np.float64, order="F")
-    qrupdate.dqrder(Q,R,j,w)
+    w = np.zeros(2 * m).astype(np.float64, order="F")
+    qrupdate.dqrder(Q, R, j, w)
 
-    Q = Q[:-1,:-1]
+    Q = Q.copy(order="C")
+    R = R.copy(order="C")
+    Q = Q[:-1, :-1]
     R = R[:-1, :]
-
-    Q = Q.copy(order='C')
-    R = R.copy(order='C')
     return Q, R
+
 
 def cholesky_update(R, j, x):
     """
@@ -48,8 +152,9 @@ def cholesky_update(R, j, x):
     x = x.astype(np.float64, order="F")
     w = np.zeros(j).astype(np.float64, order="F")
     qrupdate.dch1up(R, x, w)
-    R = R.copy(order='C')
+    R = R.copy(order="C")
     return R
+
 
 def cholesky_downdate(R, j, x):
     """
@@ -59,5 +164,5 @@ def cholesky_downdate(R, j, x):
     x = x.astype(np.float64, order="F")
     w = np.zeros(j).astype(np.float64, order="F")
     qrupdate.dch1dn(R, x, w, 0)
-    R = R.copy(order='C')
+    R = R.copy(order="C")
     return R
