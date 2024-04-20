e9049933f4072b1e3b74acec528e29321c35360c
AnZreww <129043126+An-314@users.noreply.github.com>
2024-04-20 21:10:40 +0800

    测试通过了最初版本的dlartg

--- dlartg/python/dlartg.py
+++ dlartg/python/dlartg.py
@@ -1,5 +1,9 @@
 import numpy as np
 import scipy.linalg.lapack as lapack
+import matplotlib
+
+matplotlib.use("Agg")  # 设置非交互式后端
+import matplotlib.pyplot as plt
 
 
 def dlartg(f, g):
@@ -21,28 +25,48 @@ def dlartg(f, g):
         sn = 1
     else:
         if abs(g) > abs(f):
-            tau = -f / g
+            tau = f / g
             sn = 1 / np.sqrt(1 + tau**2)
             cs = sn * tau
         else:
-            tau = -g / f
+            tau = g / f
             cs = 1 / np.sqrt(1 + tau**2)
             sn = cs * tau
     return cs, sn
 
 
 def test():
-    # 生成float64的随机数
-    np.random.seed(0)
-    f = np.random.randn()
-    g = np.random.randn()
+    # 随机生成一个1000 - 10000之间的float64
+    f = np.random.uniform(100, 1000)
+    # 随机生成一个0 - 100之间的float64
+    g = np.random.uniform(0, 1)
     print(f, g)
-
     cs1, sn1, _ = lapack.dlartg(f, g)
     cs2, sn2 = dlartg(f, g)
-    print(cs1, sn1)
-    print(cs2, sn2)
+    print(cs1 - cs2, sn1 - sn2)
+    return f, g, cs1 - cs2, sn1 - sn2
 
 
 if __name__ == "__main__":
-    test()
+    # 生成四个长为k的list
+    k = 100
+    f, g, cs, sn = np.zeros(k), np.zeros(k), np.zeros(k), np.zeros(k)
+    for i in range(k):
+        f[i], g[i], cs[i], sn[i] = test()
+    # 绘制散点图：横坐标为0-k，纵坐标为f，g
+    plt.plot(f)
+    plt.plot(g)
+    plt.xlabel("k")
+    plt.ylabel("f, g")
+    plt.legend(["f", "g"])
+    plt.title("given f and g")
+    plt.savefig("f_g.png")
+    # 绘制误差图
+    plt.cla()
+    plt.plot(cs)
+    plt.plot(sn)
+    plt.xlabel("k")
+    plt.ylabel("error")
+    plt.legend(["cs", "sn"])
+    plt.title("error of cs and sn")
+    plt.savefig("cs_sn.png")
